a10033c52a2526bb70250c11d0273fe0ec879795
sayed33 <elsayedhamdifouda@gmail.com>
2025-07-13 05:34:21 +0400

    First deploy1

--- datawaveai/settings.py
+++ datawaveai/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@vawm4bz$46b#6e&6$ljyxm4^a6)6vui*v9(y^7)aizafr&6k9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sayedhamdi.pythonanywhere.com', 'www.datawaveai.com']
 
 
 # Application definition
@@ -134,6 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website', 'static')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
